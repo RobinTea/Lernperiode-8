0d27ad7720f64171ee3d88f43f64f182b131e836
Robin <142886484+RobinTea@users.noreply.github.com>
2025-01-17 17:02:21 +0100

    Add files via upload

---
+++ 17.01.2025/ZahlEingabe1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int x = 1;
+
+int main() {
+        cout << "Gib eine Zahl ein: "; //fragt den User
+        cin >> x; //nimmt den Input
+        cout << "Deine Zahl ist: " << x << endl; //bestätigt die Zahl
+        return 0;
+}
